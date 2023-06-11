72e32be9a79909717b2b7d81d5ff3bb3109a2584
crewcik <113601819+crewcik@users.noreply.github.com>
2023-06-11 22:23:48 +0300

    Update interactionCreate.js

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -1,1 +1,19 @@
+const { InteractionType } = require("discord.js");
+const crew = require('../crewcik')
 
+module.exports = {
+    name: 'interactionCreate',
+    execute: async (interaction) => {
+        let client = interaction.client;
+        if (interaction.type == InteractionType.ApplicationCommand) {
+            if (interaction.user.bot) return;
+            try {
+                const command = client.slashcommands.get(interaction.commandName)
+                command.run(client, interaction)
+            } catch (e) {
+                console.error(e)
+                interaction.reply({ content: `Komutta bir hata var lütfen <@&${crew.crew}> ile iletişime geçn.`, ephemeral: true })
+            }
+        }
+    }
+}
