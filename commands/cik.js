01e937aba2d61e7091614160bded40c78a6faded
crewcik <113601819+crewcik@users.noreply.github.com>
2023-06-11 22:23:04 +0300

    Update cik.js

--- commands/cik.js
+++ commands/cik.js
@@ -1,1 +1,11 @@
+const { EmbedBuilder, PermissionsBitField } = require("discord.js");
+const { SlashCommandBuilder } = require("@discordjs/builders");
 
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName("ping")
+        .setDescription("Pong!"),
+    run: async (client, interaction) => {
+        interaction.reply(`Pong\nBu kod https://github.com/crewcik tarafından kodlandı.`)
+    }
+};
