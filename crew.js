2cbc3470bc68c7f3a08455fe945c8e444c4364e5
crewcik <113601819+crewcik@users.noreply.github.com>
2023-06-11 22:21:10 +0300

    Update and rename crew.js to crewcik.js

--- crew.js
+++
@@ -1,3 +0,0 @@
-let sayılar = ["1", "2", "3"];
-
-console.log(sayılar);
---
+++ crewcik.js
@@ -0,0 +1,5 @@
+module.exports = {
+    crew: "992903509701828679", // Burayı bırakın değiştirirseniz kod hata verebilir.
+    token: "Tokeninizi girin",
+    durum: "Crew tarafından kodlandı."
+}
