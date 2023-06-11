7c24fdaef886dd4f9a5ab98208e030463a9e18f1
crewcik <113601819+crewcik@users.noreply.github.com>
2023-06-11 22:20:29 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,14 +1,47 @@
-const crew = require('discord.js')
-const creWcon = require('./crew.json')
+const { Client, Collection, GatewayIntentBits, Partials } = require("discord.js");
+const client = new Client({intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMembers, GatewayIntentBits.GuildEmojisAndStickers, GatewayIntentBits.GuildIntegrations, GatewayIntentBits.GuildWebhooks, GatewayIntentBits.GuildInvites, GatewayIntentBits.GuildVoiceStates, GatewayIntentBits.GuildPresences, GatewayIntentBits.GuildMessages, GatewayIntentBits.GuildMessageReactions, GatewayIntentBits.GuildMessageTyping, GatewayIntentBits.DirectMessages, GatewayIntentBits.DirectMessageReactions, GatewayIntentBits.DirectMessageTyping, GatewayIntentBits.MessageContent], shards: "auto", partials: [Partials.Message, Partials.Channel, Partials.GuildMember, Partials.Reaction, Partials.GuildScheduledEvent, Partials.User, Partials.ThreadMember]});
+const crew = require("./crewcik");
+const { readdirSync } = require("fs")
+const moment = require("moment");
+const { REST } = require('@discordjs/rest');
+const { Routes } = require('discord-api-types/v10');
 
-let token = crewcon.token;
+let crewcik = crew.token
 
-crew.login(token)
-console.log('Token Girdi')
+client.commands = new Collection()
+client.slashcommands = new Collection()
+client.commandaliases = new Collection()
 
+const rest = new REST({ version: '10' }).setToken(crewcik);
 
-let log = console.log()
+const log = x => { console.log(`[${moment().format("DD-MM-YYYY HH:mm:ss")}] ${x}`) };
 
-let crew_cik = require('croxydb')
+const slashcommands = [];
+readdirSync('./commands').forEach(async file => {
+  const command = await require(`./commands/${file}`);
+  slashcommands.push(command.data.toJSON());
+  client.slashcommands.set(command.data.name, command);
+})
 
-// Komut test amaçlıdır..
+client.on("ready", async () => {
+        try {
+            await rest.put(
+                Routes.applicationCommands(client.user.id),
+                { body: slashcommands },
+            );
+        } catch (error) {
+            console.error(error);
+        }
+    log(`${client.user.username} Aktif Edildi!`);
+})
+
+readdirSync('./events').forEach(async file => {
+	const event = await require(`./events/${file}`);
+	if (event.once) {
+		client.once(event.name, (...args) => event.execute(...args));
+	} else {
+		client.on(event.name, (...args) => event.execute(...args));
+	}
+})
+
+client.login(crewcik)
